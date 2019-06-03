d2872face01e782a15024c039fa172b80b84efd5
sikiboo <internal.777.iky@gmail.com>
2019-06-03 13:42:20 +0700

    libur gath

--- psg2js.js
+++ psg2js.js
@@ -2,6 +2,7 @@ window.onLoad = Notification.requestPermission();
 var settingan = {
     icon: 'https://1.bp.blogspot.com/-qy5PMFersHk/XN_xkCpYuzI/AAAAAAAAHDY/lbpYyvaz1yk_oITLwfbhksVH1DNhnt8mgCLcBGAs/s320/didesign-Logo%2BKarakter%2BPontianak%2BSugar%2BGlider.png',
     body: 'Minggu, Tanggal 2-Juni-2019, di Taman alun alun kapuas, dari jam 16:00 ~ Selesai',
+    body: 'untuk sementara gath libur dulu yaa, selamat hari lebaran bagi yang merayakannya/n salam gule melayang',
     vibrate: [100, 50, 100]
 };
 
