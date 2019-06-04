5076f85f89c82d580eb84a073d1698592dbee4c0
sikiboo <internal.777.iky@gmail.com>
2019-06-04 12:55:24 +0700

    psg2style.js

--- psg2style.js
+++ psg2style.js
@@ -1,19 +1,22 @@
 $(window).scroll(function () {
     var wwscroll = $(this).scrollTop();
     console.log(wwscroll);
+
     if (wwscroll > 100) {
         $('.judul').addClass('judultambahan');
     } else {
         $('.judul').removeClass('judultambahan');
-    };
+    }
+
     if (wwscroll > 155) {
-        $('post-body').addClass('post-tambahan');
+        $('.post-body').addClass('post-tambahan');
     } else {
-        $('post-body').removeClass('post-tambahan');
-    };
+        $('.post-body').removeClass('post-tambahan');
+    }
+
     if (wwscroll > 1070) {
         $('.aboutpsg h1').addClass('judultambahan');
     } else {
         $('.aboutpsg h1').removeClass('judultambahan');
-    };
+    }
 });
