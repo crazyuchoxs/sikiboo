b1ffa448ed89ffea4efead843d4c9fb476589263
sikiboo <internal.777.iky@gmail.com>
2019-06-09 13:45:48 +0700

    tambah js 12

--- psg2style.js
+++ psg2style.js
@@ -1,6 +1,5 @@
 $(window).scroll(function () {
     var wwscroll = $(this).scrollTop();
-    console.log(wwscroll);
     if (wwscroll > 100) {
         $('.judul').addClass('judultambahan');
     } else {
