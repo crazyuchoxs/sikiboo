da87d6b3897762ba8c60f48420ab22cc30652a20
sikiboo <internal.777.iky@gmail.com>
2019-06-19 12:19:03 +0700

    edit jadwal gath

--- psg2js.js
+++ psg2js.js
@@ -1,8 +1,7 @@
 window.onLoad = Notification.requestPermission();
 var settingan = {
     icon: 'https://1.bp.blogspot.com/-qy5PMFersHk/XN_xkCpYuzI/AAAAAAAAHDY/lbpYyvaz1yk_oITLwfbhksVH1DNhnt8mgCLcBGAs/s320/didesign-Logo%2BKarakter%2BPontianak%2BSugar%2BGlider.png',
-    body: 'Minggu, Tanggal 2-Juni-2019, di Taman alun alun kapuas, dari jam 16:00 ~ Selesai',
-    body: 'untuk sementara gath libur dulu yaa, selamat hari lebaran bagi yang merayakannya. salam gule melayang',
+    body: 'Minggu, Tanggal 16-Juni-2019, di Taman Digulis, dari jam 16:00 ~ Selesai',
     vibrate: [100, 50, 100]
 };
 
