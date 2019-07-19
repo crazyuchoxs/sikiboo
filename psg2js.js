c57209a2ba34fe4a15ad58f43871d90136300bf3
sikiboo <internal.777.iky@gmail.com>
2019-07-19 18:40:27 +0700

    update gath

--- psg2js.js
+++ psg2js.js
@@ -1,7 +1,7 @@
 window.onLoad = Notification.requestPermission();
 var settingan = {
     icon: 'https://1.bp.blogspot.com/-qy5PMFersHk/XN_xkCpYuzI/AAAAAAAAHDY/lbpYyvaz1yk_oITLwfbhksVH1DNhnt8mgCLcBGAs/s320/didesign-Logo%2BKarakter%2BPontianak%2BSugar%2BGlider.png',
-    body: 'Minggu, Tanggal 14 Juli 2019, di Alun - Alun Kapuas (Korem), dari jam 16:00 ~ Selesai',
+    body: 'Minggu, Tanggal 21 Juli 2019, di Alun - Alun Kapuas (Korem), dari jam 16:00 ~ Selesai',
     vibrate: [100, 50, 100]
 };
 
