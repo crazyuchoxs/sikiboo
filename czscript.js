178de6747416dd650b6e24177cd9b6930361fd68
crazyuchoxs <36880442+crazyuchoxs@users.noreply.github.com>
2023-03-17 08:04:56 +0700

    main-post

---
+++ czscript.js
@@ -0,0 +1,9 @@
+var elements = document.getElementsByClassName('centered-bottom');
+for (var i = 0; i < elements.length; i++) {
+    elements[i].classList.add('container-fluid');
+};
+
+var elements = document.getElementsByClassName('main-container');
+for (var i = 0; i < elements.length; i++) {
+    elements[i].classList.add('col-10');
+}
