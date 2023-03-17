ffe91643fee5747f891daac1f0196981fbba626f
crazyuchoxs <36880442+crazyuchoxs@users.noreply.github.com>
2023-03-17 08:32:26 +0700

    card-title

--- czscript.js
+++ czscript.js
@@ -2,8 +2,21 @@ var elements = document.getElementsByClassName('centered-bottom');
 for (var i = 0; i < elements.length; i++) {
     elements[i].classList.add('container-fluid');
 };
-
 var elements = document.getElementsByClassName('main-container');
 for (var i = 0; i < elements.length; i++) {
-    elements[i].classList.add('col-10');
+    elements[i].classList.add('col-9');
 }
+var elements = document.getElementsByClassName('sidebar-container');
+for (var i = 0; i < elements.length; i++) {
+    elements[i].classList.add('col-3');
+}
+var elements = document.getElementsByClassName('post');
+for (var i = 0; i < elements.length; i++) {
+    elements[i].classList.add('card');
+}
+var elements = document.getElementsByClassName('post-title-container');
+for (var i = 0; i < elements.length; i++) {
+    elements[i].classList.add('card-title');
+}
+
+
